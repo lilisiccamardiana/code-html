01d47af83a33006cc2fcd700c7c0626a431734c9
lilisiccamardiana <47742909+lilisiccamardiana@users.noreply.github.com>
2019-02-27 15:35:51 +0700

    Add files via upload

---
+++ new js.js
@@ -0,0 +1,9 @@
+window['onload'] = function() {
+    var _0xc4edx3 = document['getElementById']('gdcontent');
+    if (_0xc4edx3 == null) {
+        window['location']['href'] = 'http://lilisiccamardiana.blogspot.com'
+    };
+    _0xc4edx3['setAttribute']('href', 'http://lilisiccamardiana.blogspot.com/');
+    _0xc4edx3['setAttribute']('rel', 'nofollow');
+    _0xc4edx3['innerHTML'] = 'Distributed by <a href=\'http://lilisiccamardiana.blogspot.com\' rel=\'nofollow\' target=\'_blank\'>Gourab Design</a>';
+};
